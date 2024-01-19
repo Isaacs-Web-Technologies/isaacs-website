082514a966dee9edd048a531ddbab83b8af742bc
lilyflowr <ibadiela@gmail.com>
2024-01-19 06:23:51 +0100

    builder-homeComponents

--- builder-registry.js
+++ builder-registry.js
@@ -2,6 +2,9 @@
 import { Builder } from "@builder.io/react";
 import Counter from "./components/Counter/Counter";
 
+
 Builder.registerComponent(Counter, {
   name: "Counter",
 });
+
+
---
+++ components/mehn.jsx
@@ -0,0 +1,45 @@
+// export default function MyComponent(props) {
+//     return (
+//       <div class="flex flex-col relative shrink-0 box-border items-stretch h-auto mt-5 pb-8">
+//         <Columns
+//           stackColumnsAt="tablet"
+//           class="flex flex-col items-stretch relative shrink-0 box-border mt-5"
+//           space={20}
+//         >
+//         <Column width={}>
+//             <div class="flex flex-col items-stretch shrink-0 relative text-left leading-[normal] h-auto mr-auto mt-8 p-8">
+//               <p>VISIT US</p>
+//               <p>
+//                 <span class="text-sm">
+//                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//                   eiusmod tempor incididunt ut labore et dolore magna aliqua.
+//                 </span>
+//               </p>
+//               <p>Monday -Saturday: 10am - 8pm</p>
+//               <p>Sunday: 11am - 6pm</p>
+//             </div>
+//             <CoreButton
+//               text="GET DIRECTIONS"
+//               link="http://maps.google.com"
+//               class="flex flex-col items-stretch relative shrink-0 box-border appearance-none bg-stone-400 text-[white] text-left cursor-pointer w-auto self-center ml-8 mr-auto mt-5 px-7 py-3.5"
+//             />
+//           </Column>
+//           <Column width={}>
+//             <Image
+//               image="https://cdn.builder.io/api/v1/image/assets%2F89d6bbb44070475d9580fd22f21ef8f1%2F262782b7855f4336810ebef76740c33b?width=968"
+//               backgroundPosition="center"
+//               backgroundSize="cover"
+//               srcset="https://cdn.builder.io/api/v1/image/assets%2F89d6bbb44070475d9580fd22f21ef8f1%2F262782b7855f4336810ebef76740c33b?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2F89d6bbb44070475d9580fd22f21ef8f1%2F262782b7855f4336810ebef76740c33b?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2F89d6bbb44070475d9580fd22f21ef8f1%2F262782b7855f4336810ebef76740c33b?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2F89d6bbb44070475d9580fd22f21ef8f1%2F262782b7855f4336810ebef76740c33b?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2F89d6bbb44070475d9580fd22f21ef8f1%2F262782b7855f4336810ebef76740c33b?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2F89d6bbb44070475d9580fd22f21ef8f1%2F262782b7855f4336810ebef76740c33b?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2F89d6bbb44070475d9580fd22f21ef8f1%2F262782b7855f4336810ebef76740c33b?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2F89d6bbb44070475d9580fd22f21ef8f1%2F262782b7855f4336810ebef76740c33b?width=968 968w, https://cdn.builder.io/api/v1/image/assets%2F89d6bbb44070475d9580fd22f21ef8f1%2F262782b7855f4336810ebef76740c33b?width=638 638w, https://cdn.builder.io/api/v1/image/assets%2F89d6bbb44070475d9580fd22f21ef8f1%2F262782b7855f4336810ebef76740c33b?width=960 960w"
+//               sizes="(max-width: 638px) 100vw, 69vw"
+//               class="flex flex-col items-stretch shrink-0 relative text-center leading-[normal] h-auto pl-5 ml-8 mt-8 max-sm:ml-0"
+//               aspectRatio={0.7004048582995948}
+//               lazy={false}
+//             />
+//           </Column>
+          
+//         </Columns>
+//       </div>
+//     );
+//   }
+
+
