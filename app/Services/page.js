502bb245ddb930a3ba319f1c65faa26367f2bade
lilyflowr <ibadiela@gmail.com>
2024-01-22 03:20:44 +0100

    serve

--- app/Services/page.js
+++ app/Services/page.js
@@ -9,7 +9,7 @@ const page = () => {
     <>
     <section className=''>
     <div className="items-stretch  bg-fuchsia-700 flex w-full flex-col mb-6 justify-center mx-auto px-12 py-12 max-md:max-w-full max-md:px-5">
-            <div className="text-white text-4xl text-center font-extrabold leading-10 grow mt-40 max-md:max-w-full">
+            <div className="text-white text-4xl text-center font-extrabold leading-10 grow mt-10 max-md:max-w-full">
                  Services
             </div>
         </div>
--- app/page.js
+++ app/page.js
@@ -3,9 +3,7 @@ import Awesome from '../components/HomeComponents/Awesome/Awesome'
 import ClientCarousel from '../components/HomeComponents/ClientCarousel/ClientCarousel'
 import Contact from '../components/HomeComponents/Contact/Contact'
 import DigitalMarketing from '../components/HomeComponents/DigitalMarketing/DigitalMarketing'
-import Footer from '../components/HomeComponents/Footer/Footer'
 import FullServiceDigi from '../components/HomeComponents/FullServiceDigi/FullServiceDigi'
-import Navbar from '../components/HomeComponents/Navbar/Navbar'
 import Purpleseo from '../components/HomeComponents/Purpleseo/Purpleseo'
 import Smallpple from '../components/HomeComponents/Smallpple/Smallpple'
 import StandingDigi from '../components/HomeComponents/StandingDigi/StandingDigi'
@@ -18,7 +16,7 @@ import RollinNos from '../components/HomeComponents/RollingNos/RollingNos'
 export default function Home() {
   return (
     <>
-    <Navbar />
+   
     <Hero /> 
     <DigitalMarketing /> 
     <Smallpple />
@@ -31,7 +29,6 @@ export default function Home() {
     <ClientCarousel />
     <Contact />
     <Purpleseo/>
-    <Footer />
    
     </>
    
