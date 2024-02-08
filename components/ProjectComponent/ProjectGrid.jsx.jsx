14d8f7c92f7c70dc04fff36f9f4a6ede92489166
lilyflowr <ibadiela@gmail.com>
2024-02-09 00:22:11 +0100

    project anime

--- components/ProjectComponent/ProjectGrid.jsx.jsx
+++ components/ProjectComponent/ProjectGrid.jsx.jsx
@@ -1,121 +1,69 @@
 const ProjectGrid = () => {
+  const imageData = [
+    {
+      id: 1,
+      imageUrl: '/img/portfolio1.png',
+      title: 'Image 1',
+      description: 'Short description for image 1',
+      detailedText: 'Detailed text for image 1',
+    },
+    {
+      id: 2,
+      imageUrl: '/img/portfolio2.png',
+      title: 'Image 2',
+      description: 'Short description for image 2',
+      detailedText: 'Detailed text for image 2',
+    },
+    {
+      id: 3,
+      imageUrl: '/img/portfolio3.png',
+      title: 'Image 3',
+      description: 'Short description for image 3',
+      detailedText: 'Detailed text for image 3',
+    },
+    {id: 4,
+      imageUrl: '/img/portfolio4.png',
+      title: 'Image 4',
+      description: 'Short description for image 1',
+      detailedText: 'Detailed text for image 1',
+    },
+    {
+      id: 5,
+      imageUrl: '/img/portfolio5.png',
+      title: 'Image 5',
+      description: 'Short description for image 3',
+      detailedText: 'Detailed text for image 2',
+    },
+    {
+      id: 6,
+      imageUrl: '/img/portfolio6.png',
+      title: 'Image 6',
+      description: 'Short description for image 3',
+      detailedText: 'Detailed text for image 3',
+    },
+  ];
   return (
-        <section className="flex flex-col mb-4 items-stretch">
-{/*ist row 1  */}
-          <div className="w-full px-5 max-md:max-w-full gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-           
-              <div className="flex  flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
-                <div className="items-center shadow-lg flex grow flex-col text-base text-center pl-6 pr-12 py-12 rounded-xl max-md:max-w-full max-md:mt-10 max-md:px-5">
-                  <img loading="lazy" 
-                  src="/img/portfolio1.png"
-                  className="aspect-[0.91] object-contain object-center w-full self-stretch" 
-                  />
-                  <h5 className="text-neutral-700 leading-[169%] tracking-wide whitespace-nowrap mt-2.5">Search Optimization</h5>
-                  <p className="text-gray-500 leading-6 w-[280px] mt-2.5">Proin hendrerit vestibulum ipsum non malesuada. Nunc ut sem cursus nisl tincidunt dictum et vel eros.</p>
-                   {/* Hidden content */}
-                   {/* <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold">
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae facilis accusantium mollitia eligendi illum consequatur laborum vel culpa,
-                    facere rem totam! Distinctio iure vero in qui nulla voluptates veniam maiores!
-                   </div> */}
-                  
-                  {/* End of hidden content */}
-                </div>
-              </div>
-              {/* 2 */}
-              <div className="flex flex-col items-stretch w-[35%] ml-5 max-md:w-full max-md:ml-0">
-                <div className="items-center shadow-lg flex grow flex-col text-base text-center px-12 py-12 rounded-xl max-md:max-w-full max-md:mt-10 max-md:px-5">
-                  <img loading="lazy" 
-                  src="/img/portfolio2.png"
-                  className="aspect-[0.91] object-contain object-center w-full self-stretch" />
-                  <h5 className="text-neutral-700 leading-[169%] tracking-wide whitespace-nowrap mt-2.5">Search Optimization</h5>
-                  <p className="text-gray-500 leading-6 w-[280px] mt-2.5">Proin hendrerit vestibulum ipsum non malesuada. Nunc ut sem cursus nisl tincidunt dictum et vel eros.</p>
-                   {/* Hidden content */}
-                   {/* <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold">
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae facilis accusantium mollitia eligendi illum consequatur laborum vel culpa,
-                    facere rem totam! Distinctio iure vero in qui nulla voluptates veniam maiores!
-                   </div> */}
-                  
-                  {/* End of hidden content */}
-                </div>
-              </div>
-              {/* 3 */}
-              <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
-                <div className="items-center shadow-lg flex grow flex-col text-base text-center pl-12 pr-6 py-12 rounded-xl max-md:max-w-full max-md:mt-10 max-md:px-5">
-                  <img loading="lazy" 
-                  src="/img/portfolio3.png"
-                  className="aspect-[0.91] object-contain object-center w-full self-stretch" />
-                  <h5 className="text-neutral-700 leading-[169%] tracking-wide whitespace-nowrap mt-2.5">Search Optimization</h5>
-                  <p className="text-gray-500 leading-6 w-[280px] mt-2.5">Proin hendrerit vestibulum ipsum non malesuada. Nunc ut sem cursus nisl tincidunt dictum et vel eros.</p>
-                 {/* Hidden content */}
-                 {/* <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold">
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae facilis accusantium mollitia eligendi illum consequatur laborum vel culpa,
-                    facere rem totam! Distinctio iure vero in qui nulla voluptates veniam maiores!
-                   </div> */}
-                  
-                  {/* End of hidden content */}</div>
-              </div>
-           
+    <div className="grid grid-cols-1 my-4  sm:grid-cols-2 md:grid-cols-3 gap-4 shadow-lg ">
+      {imageData.map((image) => (
+        <div key={image.id} className=" bg-white relative mx-2 rounded-xl shadow-lg  group object-contain">
+          <img
+            src={image.imageUrl}
+            alt={image.title}
+            className="w-full h-auto cursor-pointer transition-all duration-300 transform group-hover:scale-105"
+          />
+          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <div className="text-white ">
+              <h3 className="text-lg font-bold">{image.title}</h3>
+              <p className="text-sm bottom-0 left-0 ">{image.description}</p>
+              <p className="text-xs hidden group-hover:block">{image.detailedText}</p>
+            </div>
           </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-{/* second row,1 */}
-          <div className="w-full mt-12 px-5 max-md:max-w-full max-md:mt-10 gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-           
-              <div className="flex flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
-                <div className="items-center shadow-lg flex grow flex-col text-base text-center pl-6 pr-12 py-12 rounded-xl max-md:max-w-full max-md:mt-10 max-md:px-5">
-                  <img loading="lazy" 
-                  src="/img/portfolio4.png"
-                  className="aspect-[0.91] object-contain object-center w-full self-stretch" />
-                  <h5 className="text-neutral-700 leading-[169%] tracking-wide whitespace-nowrap mt-2.5" aria-label="Search Optimization">Search Optimization</h5>
-                  <p className="text-gray-500 leading-6 w-[280px] mt-2.5">Proin hendrerit vestibulum ipsum non malesuada. Nunc ut sem cursus nisl tincidunt dictum et vel eros.</p>
-                 {/* Hidden content */}
-                 {/* <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold">
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae facilis accusantium mollitia eligendi illum consequatur laborum vel culpa,
-                    facere rem totam! Distinctio iure vero in qui nulla voluptates veniam maiores!
-                   </div> */}
-                  
-                  {/* End of hidden content */}
-                </div>
-              
-              </div>
-                {/* 2 */}
-              <div className="flex flex-col items-stretch w-[35%] ml-5 max-md:w-full max-md:ml-0">
-                <div className="items-center shadow-lg flex grow flex-col text-base text-center px-12 py-12 rounded-xl max-md:max-w-full max-md:mt-10 max-md:px-5">
-                  <img loading="lazy" 
-                  src="/img/portfolio5.png"
-                  className="aspect-[0.91] object-contain object-center w-full self-stretch" />
-                  <h5 className="text-neutral-700 leading-[169%] tracking-wide whitespace-nowrap mt-2.5" aria-label="Search Optimization">Search Optimization</h5>
-                  <p className="text-gray-500 leading-6 w-[280px] mt-2.5">Proin hendrerit vestibulum ipsum non malesuada. Nunc ut sem cursus nisl tincidunt dictum et vel eros.</p>
-                   {/* Hidden content */}
-                   {/* <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold">
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae facilis accusantium mollitia eligendi illum consequatur laborum vel culpa,
-                    facere rem totam! Distinctio iure vero in qui nulla voluptates veniam maiores!
-                   </div> */}
-                  
-                  {/* End of hidden content */}
-                </div>
-              </div>
-              {/* 3 */}
-              <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
-                <div className="items-center shadow-lg flex grow flex-col text-base text-center pl-12 pr-6 py-12 rounded-xl max-md:max-w-full max-md:mt-10 max-md:px-5">
-                  <img loading="lazy" 
-                  src="/img/portfolio6.png"
-                  className="aspect-[0.91] object-contain object-center w-full self-stretch" />
-                  <h5 className="text-neutral-700 leading-[169%] tracking-wide whitespace-nowrap mt-2.5" aria-label="Search Optimization">Search Optimization</h5>
-                  <p className="text-gray-500 leading-6 w-[280px] mt-2.5">Proin hendrerit vestibulum ipsum non malesuada. Nunc ut sem cursus nisl tincidunt dictum et vel eros.</p>
-                   {/* Hidden content */}
-                   {/* <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold">
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae facilis accusantium mollitia eligendi illum consequatur laborum vel culpa,
-                    facere rem totam! Distinctio iure vero in qui nulla voluptates veniam maiores!
-                   </div>
-                   */}
-                  {/* End of hidden content */}
-                </div>
-              </div>
-          
-          </div>
-        </section>
-      );
-    }
 
 
 export default ProjectGrid
